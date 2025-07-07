761b7993d0f0c52d1fc59b87330875cc482860f8
snehamahat211 <snehamahat211@gmail.com>
2025-07-07 18:55:07 +0545

    socket creation and configuration

--- client.c
+++ client.c
@@ -13,6 +13,15 @@ int main() {
     char buffer[BUFFER_SIZE]; 
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
     printf("WSAStartup failed. Error Code: %d\n", WSAGetLastError());
-    return 1; }  
-         
+    return 1; } 
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == INVALID_SOCKET) {
+    printf("Socket creation failed. Error Code: %d\n", WSAGetLastError());
+    WSACleanup();
+    return 1;}
+     server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+
 
