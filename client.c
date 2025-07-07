36c4fab1f94c467266b0e5f1496a34c51f685d10
snehamahat211 <snehamahat211@gmail.com>
2025-07-07 18:49:21 +0545

    winshock initialize

--- client.c
+++ client.c
@@ -3,3 +3,16 @@
 #include <string.h>
 #include <winsock2.h>
 #include <ctype.h>
+#pragma comment(lib, "ws2_32.lib") 
+#define PORT 8080          
+#define BUFFER_SIZE 1024  
+int main() {
+    WSADATA wsa;                      
+    SOCKET client_socket;             
+    struct sockaddr_in server_addr;   
+    char buffer[BUFFER_SIZE]; 
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+    printf("WSAStartup failed. Error Code: %d\n", WSAGetLastError());
+    return 1; }  
+         
+
